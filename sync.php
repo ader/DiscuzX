b9972539ba57df71f480a85c3127f2989b7ba0ce
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-03-28 09:11:11 +0800

    同步模块

--- sync.php
+++ sync.php
@@ -1,12 +1,17 @@
 <?php
 echo <<<HTML
+<!DocType HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style>
 body{
 font-size:12px;
 }
 </style>
 HTML;
-
+require_once 'functions.php';
+require_once 'pclzip.class.php';
 if (@$_REQUEST['operation'] == 'push') {
 	if ($_FILES["file"]["error"] > 0) {
 		echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
@@ -23,13 +28,42 @@ if (@$_REQUEST['operation'] == 'push') {
 				"./" . $_FILES["file"]["name"]);
 			echo "Stored in: " . "./" . $_FILES["file"]["name"];
 
-			require_once 'zip.class.php';
-			$unzip = new Unzip();
-			$unzip->Extract('package.zip', '.');
-			if ($result == -1) {
-				echo "<br>文件 $upfile[name] 错误.<br>";
+			$path = './';
+			$name = 'package.zip';
+			$remove = 0;
+			$unzippath = './';
+			if (file_exists($path . $name) && is_file($path . $name)) {
+				$Zip = new PclZip($path . $name);
+				$result = $Zip->extract($path . (('./' == $unzippath || '。/' == @$_POST['unzippath']) ? '' : $unzippath), $remove);
+				if ($result) {
+					exit('skl');
+					$statusCode = 200;
+					$list = $Zip->listContent();
+					$fold = 0;
+					$fil = 0;
+					$tot_comp = 0;
+					$tot_uncomp = 0;
+					foreach ($list as $key => $val) {
+						if ($val['folder'] == '1') {
+							++$fold;
+						} else {
+							++$fil;
+							$tot_comp += $val['compressed_size'];
+							$tot_uncomp += $val['size'];
+						}
+					}
+					$message = '<font color="green">解压目标文件：</font><font color="red"> ' . g2u($name) . '</font><br />';
+					$message .= '<font color="green">解压文件详情：</font><font color="red">共' . $fold . ' 个目录，' . $fil . ' 个文件</font><br />';
+					$message .= '<font color="green">压缩文档大小：</font><font color="red">' . dealsize($tot_comp) . '</font><br />';
+					$message .= '<font color="green">解压文档大小：</font><font color="red">' . dealsize($tot_uncomp) . '</font><br />';
+					$message .= '<font color="green">解压总计耗时：</font><font color="red">' . G('_run_start', '_run_end', 6) . ' 秒</font><br />';
+				} else {
+					exit('error');
+					$statusCode = 300;
+					$message .= '<font color="blue">解压失败：</font><font color="red">' . $Zip->errorInfo(true) . '</font><br />';
+					$message .= '<font color="green">执行耗时：</font><font color="red">' . G('_run_start', '_run_end', 6) . ' 秒</font><br />';
+				}
 			}
-			echo "<br>完成,共建立 $unzip->total_folders 个目录,$unzip->total_files 个文件.<br><br><br>";
 		}
 	}
 } elseif (@$_REQUEST['operation'] == 'unzip') {
@@ -38,7 +72,6 @@ if (@$_REQUEST['operation'] == 'push') {
 	$remove = 0;
 	$unzippath = './';
 	if (file_exists($path . $name) && is_file($path . $name)) {
-		include('pclzip.class.php');
 		$Zip = new PclZip($path . $name);
 		$result = $Zip->extract($path . (('./' == $unzippath || '。/' == @$_POST['unzippath']) ? '' : $unzippath), $remove);
 		if ($result) {
@@ -71,51 +104,51 @@ if (@$_REQUEST['operation'] == 'push') {
 		}
 	}
 } elseif (@$_REQUEST['operation'] == 'md5') {
-	if ($_FILES["file"]["error"] > 0) {
-		echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-	} else {
-		echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-		echo "Type: " . $_FILES["file"]["type"] . "<br />";
-		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-		if (file_exists("upload/" . $_FILES["file"]["name"])) {
-			echo $_FILES["file"]["name"] . " already exists. ";
-		} else {
-			move_uploaded_file($_FILES["file"]["tmp_name"], "./data/custommd5/" . $_FILES["file"]["name"]);
-			echo "Stored in: " . "./data/custommd5/" . $_FILES["file"]["name"];
-		}
-	}
-	echo '<br /><br />';
-	$md5 = file_get_contents("./data/custommd5/" . $_FILES["file"]["name"]);
-	$md5 = explode("\n", trim($md5));
+//	if ($_FILES["file"]["error"] > 0) {
+//		echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+//	} else {
+//		echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+//		echo "Type: " . $_FILES["file"]["type"] . "<br />";
+//		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+//		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+//
+//		if (file_exists("upload/" . $_FILES["file"]["name"])) {
+//			echo $_FILES["file"]["name"] . " already exists. ";
+//		} else {
+//			move_uploaded_file($_FILES["file"]["tmp_name"], "./data/custommd5/" . $_FILES["file"]["name"]);
+//			echo "Stored in: " . "./data/custommd5/" . $_FILES["file"]["name"];
+//		}
+//	}
+//	echo '<br /><br />';
+//	$md5 = file_get_contents("./data/custommd5/" . $_FILES["file"]["name"]);
+//	$md5 = explode("\n", trim($md5));
 //	$md5 = implode('<br />', $md5);
 //	echo $md5;
 echo '<form action="sync.php?operation=md5checkedsync" method="post">';
 	$ignorelist = file_get_contents('./data/custommd5/ignorelist.txt');
 	$ignorelist = explode("\n", trim($ignorelist));
-	foreach ($md5 as $item) {
-		$item = explode(' *', $item);
-		if(in_array($item[1], $ignorelist))continue;
-		if (file_exists($item[1])) {
-			if (md5_file($item[1]) != $item[0]) {
+	foreach ($_POST['file'] as $file => $md5) {
+//		$item = explode(' *', $item);
+		if(in_array($file, $ignorelist))continue;
+		if (file_exists($file)) {
+			if (md5_file($file) != $md5) {
 				echo <<<HTML
-<input type="radio" name="file[$item[1]]" value="ignore" />忽略
-<input type="radio" name="file[$item[1]]" value="upload" />上传
-<input type="radio" name="file[$item[1]]" value="dnload" />下载
-<input type="radio" name="file[$item[1]]" value="delete" />删除
+<input type="radio" name="file[$file]" value="ignore" />忽略
+<input type="radio" name="file[$file]" value="upload" />上传
+<input type="radio" name="file[$file]" value="dnload" />下载
+<input type="radio" name="file[$file]" value="delete" />删除
 HTML;
 
-				echo '文件：' . $item[1] . ';<br />';
+				echo '文件：' . ($file) . ';<br />';
 			}
 		} else {
 			echo <<<HTML
-<input type="radio" name="file[$item[1]]" value="ignore" />忽略
-<input type="radio" name="file[$item[1]]" value="upload" />上传
-<input type="radio" name="file[$item[1]]" value="dnload" disabled />下载
-<input type="radio" name="file[$item[1]]" value="delete" />删除本地
+<input type="radio" name="file[$file]" value="ignore" />忽略
+<input type="radio" name="file[$file]" value="upload" />上传
+<input type="radio" name="file[$file]" value="dnload" disabled />下载
+<input type="radio" name="file[$file]" value="delete" />删除本地
 HTML;
-			echo '文件：' . $item[1] . '不存在！！<br />';
+			echo '文件：' . ($file) . '不存在！！<br />';
 		}
 	}
 	echo '<input type="submit" value="submit" name="submit" />';
