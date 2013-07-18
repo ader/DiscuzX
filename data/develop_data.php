09add17320353200e67e46adbb010241c820994a
SprayHank <fantasyoui@gmail.com>
2013-07-18 22:57:33 +0800

    保存资源

---
+++ data/develop_data.php
@@ -0,0 +1,22 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+
+$develop_list = array (
+  0 => 'dps_projectsupport',
+  1 => 'dps_weixinpublic',
+  2 => 'dps_qrcode',
+  3 => 'dps_accounts_oath',
+  4 => 'dps_xtao',
+  5 => 'dps_sinaweibo',
+  6 => 'dps_tencent',
+  7 => 'dps_taobao',
+  8 => 'dps_baidu',
+  9 => 'dps_google',
+  10 => 'dps_xunlei',
+  11 => 'dps_gravatar',
+  12 => 'dps_header',
+  13 => 'Vizto_discount',
+  14 => 'dps_sina',
+);
+
+?>
